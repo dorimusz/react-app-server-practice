124bff46ee91cd72b623499ae09143f3561bf36f
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-02-17 11:13:34 +0100

    login, add new todo for logged in user

--- index.js
+++ index.js
@@ -18,12 +18,60 @@ app.post('/api/signup', (req, res) => {
     }
     const newUser = {
         name: req.body.name,
-        password: req.body.password
+        password: req.body.password,
+        todos: []
     }
     users.push(newUser)
 
     fs.writeFileSync('users.json', JSON.stringify(users, null, 4))
     res.sendStatus(200)
+})
+
+app.post('/api/todo', (req, res) => {
+    const autoHead = req.header('authorization');
+    if (!autoHead) return res.sendStatus(401);
+
+    const username = autoHead.split(':::')[0];
+    const password = autoHead.split(':::')[1];
+
+    const user = users.find((user) => user.name === username && user.password === password);
+    if (!user) return res.sendStatus(401);
+
+    const todoMess = req.body.todo
+    if (!todoMess) return res.sendStatus(400)
+
+    user.todos.push(todoMess)
+    fs.writeFileSync('users.json', JSON.stringify(users, null, 4))
+    res.sendStatus(200)
+})
+
+app.get('/api/todo', (req, res) => {
+    const autoHead = req.header('authorization');
+    if (!autoHead) return res.sendStatus(401);
+
+    const username = autoHead.split(':::')[0];
+    const password = autoHead.split(':::')[1];
+
+    const user = users.find((user) => user.name === username && user.password === password);
+    if (!user) return res.sendStatus(401);
+
+    res.json(user.todos);
+    // console.log(autoHead)
+})
+
+
+app.post('/api/login', (req, res) => {
+    const autoHead = req.header('authorization')
+    if (!autoHead) return res.sendStatus(401);
+
+    const username = autoHead.split(':::')[0];
+    const password = autoHead.split(':::')[1];
+
+    const user = users.find((user) => user.name === username && user.password === password);
+    if (!user) return res.sendStatus(401);
+
+    res.sendStatus(200)
+
 })
 
 app.listen(port, () => {
