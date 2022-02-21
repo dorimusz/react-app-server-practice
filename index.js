277c1314339f27fb146cbb6b9a5d326514c1fe7e
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-02-21 11:18:08 +0100

    authorization

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const fs = require('fs');
+const { setInterval } = require('timers/promises');
 app.use(cors());
 app.use(express.json());
 
 const users = require('./users.json');
+const mySessionStorage = {};
 
 app.post('/api/signup', (req, res) => {
     if (!req.body.name || !req.body.password) {
@@ -28,13 +30,17 @@ app.post('/api/signup', (req, res) => {
 })
 
 app.post('/api/todo', (req, res) => {
-    const autoHead = req.header('authorization');
-    if (!autoHead) return res.sendStatus(401);
+    const sessionId = req.header('authorization');
+    if (!sessionId) return res.sendStatus(401);
 
+    /* //Már nincs a headerben username meg jelszó
     const username = autoHead.split(':::')[0];
     const password = autoHead.split(':::')[1];
+    */
 
-    const user = users.find((user) => user.name === username && user.password === password);
+    const user = mySessionStorage[sessionId]
+
+    //const user = users.find((user) => user.name === username && user.password === password);
     if (!user) return res.sendStatus(401);
 
     const todoMess = req.body.todo
@@ -46,19 +52,14 @@ app.post('/api/todo', (req, res) => {
 })
 
 app.get('/api/todo', (req, res) => {
-    const autoHead = req.header('authorization');
-    if (!autoHead) return res.sendStatus(401);
-
-    const username = autoHead.split(':::')[0];
-    const password = autoHead.split(':::')[1];
-
-    const user = users.find((user) => user.name === username && user.password === password);
+    const sessionId = req.header('authorization');
+    if (!sessionId) return res.sendStatus(401);
+    const user = mySessionStorage[sessionId]
     if (!user) return res.sendStatus(401);
 
     res.json(user.todos);
     // console.log(autoHead)
 })
-
 
 app.post('/api/login', (req, res) => {
     const autoHead = req.header('authorization')
@@ -69,9 +70,28 @@ app.post('/api/login', (req, res) => {
 
     const user = users.find((user) => user.name === username && user.password === password);
     if (!user) return res.sendStatus(401);
+    //res.sendStatus(200)
 
+    const sessionId = Math.random().toString(); //random string
+    mySessionStorage[sessionId] = user;
+    console.log(mySessionStorage);
+
+    setTimeout(() => {
+        console.log('Session ended');
+        delete mySessionStorage[sessionId];
+    }, 10 * 60 * 1000)
+
+    res.json(sessionId) //ez lesz ebben az esetben a response.data
+})
+
+app.delete("/api/logout", (req, res) => {
+    console.log(req.headers)
+    const sessionId = req.header('authorization');
+    console.log(sessionId)
+    if (!sessionId) return res.sendStatus(401);
+    delete mySessionStorage[sessionId];
+    console.log(mySessionStorage)
     res.sendStatus(200)
-
 })
 
 app.listen(port, () => {
